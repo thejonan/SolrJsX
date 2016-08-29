082f042b973036b991ae3e850cc0dc4fdbbfd65b
theJonan <ivan@jonan.info>
2016-08-29 21:23:48 +0300

    - First testable version.

--- src/Configuring.js
+++ src/Configuring.js
@@ -1,19 +1,7 @@
+/**
+  * Parameter specification: https://cwiki.apache.org/confluence/display/solr/Local+Parameters+in+Queries
+  */
 (function (Solr, a$) {
-  Solr.Configuring = function (obj) {
-    a$.extend(true, this, obj);
-    
-    // Now make some reformating of initial parameters.
-    var pars = this.parameters,
-        self = this;
-    this.parameters = {};
-    a$.each(pars, function (p, name) {
-      // This is relying on existence of some Querying skill.
-      p = self.parseValue(p);
-      p.name = name;
-      self.addParameter(p);
-    });
-  };
-
   /** This is directly copied from AjaxSolr.
     */  
   Solr.quoteValue = function (value) {
@@ -26,8 +14,58 @@
     return value;
   };
   
+  Solr.parseParameter = function (str) {
+    var param = { },
+        parse = str.match(/^([^=]+)=(?:\{!([^\}]*)\})?(.*)$/);
+    if (parse) {
+
+      if (parse[2] != null) {
+        var matches;
+        while (matches = /([^\s=]+)=?(\S*)?/g.exec(parse[2])) {
+          if (param.locals === undefined)
+            param.locals = {};
+          if (matches[2] == null)
+            param.locals['type'] = matches[1];
+          else
+            param.locals[matches[1]] = matches[2];
+          parse[2] = parse[2].replace(matches[0], ''); // Safari's exec seems not to do this on its own
+        }
+      }
+
+      if (parse[1] == 'q.alt') {
+        // if q.alt is present, assume it is because q was empty, as above
+        param.name = 'q';
+      }
+      else {
+        param.name = parse[1];
+        var arr = parse[3].split(",");
+        param.value = arr.length > 1 ? arr : parse[3];
+      }
+    }
+    
+    return param;
+  };
+  
+  Solr.Configuring = function (obj) {
+    a$.extend(true, this, obj);
+    
+    // Now make some reformating of initial parameters.
+    var self = this;
+        
+    this.parameterStore = {};
+    a$.each(this.parameters, function (p, name) {
+      if (typeof p === 'string')
+        self.addParameter(Solr.parseParameter(name + '=' + p));
+      else
+        self.addParameter(name, p);
+    });
+    // We no longer need this - free them.
+    delete obj.parameters;
+    delete this.parameters;
+  };
+
   var paramIsMultiple = function (name) { 
-    return name.match(/^(?:bf|bq|facet\.date|facet\.date\.other|facet\.date\.include|facet\.field|facet\.pivot|facet\.range|facet\.range\.other|facet\.range\.include|facet\.query|fq|group\.field|group\.func|group\.query|pf|qf)$/);
+    return name.match(/^(?:bf|bq|facet\.date|facet\.date\.other|facet\.date\.include|facet\.field|facet\.pivot|facet\.range|facet\.range\.other|facet\.range\.include|facet\.query|fq|group\.field|group\.func|group\.query|pf|qf|stats\.field)$/);
   };
   
   Solr.Configuring.prototype = {
@@ -45,19 +83,19 @@
         name = param.name;
       
       if (paramIsMultiple(name)) {
-        if (this.parameters[name] === undefined)
-          this.parameters[name] = param;
+        if (this.parameterStore[name] === undefined)
+          this.parameterStore[name] = param;
         else {
           var found = false;
-          a$.each(this.parameters[name], function (p) { found = found || a$.equal(true, param, p); });
+          a$.each(this.parameterStore[name], function (p) { found = found || a$.equal(true, param, p); });
           if (!found)
-            this.parameters[name].push(param);
+            this.parameterStore[name].push(param);
           else
             return false;
         }
       }
       else
-        this.parameters[name] = param;
+        this.parameterStore[name] = param;
         
       return param;
     },
@@ -67,14 +105,14 @@
       */
     findParameter: function (name, needle) {
       var indices = [];
-      if (this.parameters[name] !== undefined) {
+      if (this.parameterStore[name] !== undefined) {
         if (paramIsMultiple(name)) {
-          a$.each(this.parameters[name], function (p, i) {
+          a$.each(this.parameterStore[name], function (p, i) {
             if (a$.match(p.value))
               indices.push(i);
           });
         }
-        else if (a$.match(this.parameters[name].value, needle))
+        else if (a$.match(this.parameterStore[name].value, needle))
           indices.push(0);
       }
       return indices;
@@ -84,20 +122,20 @@
       * if not - it is first passed to findParameter() call.
       */
     removeParameters: function (name, indices) {
-      if (this.parameters[name] !== undefined) {
+      if (this.parameterStore[name] !== undefined) {
         if (!Array.isArray(indices))
           indices = this.findParameter(indices);
         
         if (paramIsMultiple(name)) {
-          if (indices.length < this.parameters[name].length) {
+          if (indices.length < this.parameterStore[name].length) {
             for (var i = 0, il = indices.length; i < il; ++i)
-              this.parameters.splice(indices[i], 1);
+              this.parameterStore.splice(indices[i], 1);
           }
           else
-            delete this.parameters[name];  
+            delete this.parameterStore[name];  
         }
         else if (indices.length > 0) {
-          delete this.parameters[name];
+          delete this.parameterStore[name];
         }
           
         return indices.length;
@@ -109,19 +147,19 @@
     /** Returns a parameter or an array of parameters with that name
       */
     getParameter: function (name) {
-      if (this.parameters[name] === undefined) {
+      if (this.parameterStore[name] === undefined) {
         var param = { 'name': name };
-        this.parameters[name] = paramIsMultiple(name) ? [ param ] : param;
+        this.parameterStore[name] = paramIsMultiple(name) ? [ param ] : param;
       }
-      return this.parameters[name];
+      return this.parameterStore[name];
     },
     
     /** Returns an array of values of all parameters with given name
       */
     getAllValues: function (name) {
       var val = null;
-      if (this.parameters[name] !== undefined)
-        val = !paramIsMultiple(name) ? this.parameters[name].value : this.parameters[name].map(function (p) { return p.value; });
+      if (this.parameterStore[name] !== undefined)
+        val = !paramIsMultiple(name) ? this.parameterStore[name].value : this.parameterStore[name].map(function (p) { return p.value; });
 
       return val;
     }
--- src/QueryingURL.js
+++ src/QueryingURL.js
@@ -3,8 +3,16 @@
     a$.extend(true, this, obj);
   };
 
-  var paramValue = function (value) { 
-    return Array.isArray(value) ? value.join(',') : value; 
+  var paramValue = function (value) {
+    if (Array.isArray(value))
+      return value.join(",");
+    else if (typeof value !== 'object')
+      return value.toString(); 
+    else {
+      var str = [];
+      a$.each(value, function (v, k) { str.push(k + ":" + Solr.quoteValue(v)); });
+      return str.join(" ");
+    }
   }
   
   Solr.QueryingURL.prototype = {
@@ -14,28 +22,28 @@
       var self = this,
           query = [];
           
-      a$.each(self.parameters, function (plist, name) {
+      a$.each(self.parameterStore, function (plist, name) {
         if (!Array.isArray(plist)) plist = [plist];
         a$.each(plist, function (param) {
           var prefix = [];
               
-          a$.each(param.locals, function (l, k) { prefix.push(k + "=" + l); })
+          a$.each(param.locals, function (l, k) { prefix.push(k + (l != null ? "=" + l : "")); })
           prefix = prefix.length > 0 ? "{!" + prefix.join(" ") + "}" : "";
           
           if (param.value)
-            query.push(name + "=" + prefix + paramValue(param.value));
+            query.push(name + "=" + encodeURIComponent(prefix + paramValue(param.value)));
 
           // For dismax request handlers, if the q parameter has local params, the
           // q parameter must be set to a non-empty value. In case the q parameter
           // has local params but is empty, use the q.alt parameter, which accepts
           // wildcards.
           else if (name == 'q' && prefix) {
-            query.push('q.alt=' + prefix + '*:*');
+            query.push('q.alt=' + encodeURIComponent(prefix + '*:*'));
           }
         });
       });
       
-      return { url: query.map(function (p) { return encodeURIComponent(p); }).join("&") };
+      return { url: '?' + query.join("&") };
     },
     
     parseQuery: function (response) {
