0ce9b20f543a038c06e62f49849f313a149c084a
theJonan <ivan@jonan.info>
2016-09-13 00:42:27 +0300

    - Testing migrated to Jasmine.

---
+++ test/SolrJs2Spec.js
@@ -0,0 +1,25 @@
+var SolrJs2 = require("../"),
+		_ = require("underscore"),
+		customMatchers = {
+			toDeepEqual: function (util, customEqualityTesters) {
+				return {
+					compare: function(actual, expected) {
+							return { pass: _.isEqual(actual, expected) };
+					}
+				}
+			}
+		};
+
+
+describe("SolrJs2:", function () {
+	// prepare the test for dual runs - browser & npm
+	beforeEach(function () {
+		var jself = typeof this.addMatchers === 'function' ? this : jasmine;
+		jself.addMatchers(customMatchers);
+	});
+
+	// Now - GO with the tests.
+	it("Trivial test", function () {
+		expect(true).toBe(true);
+	});
+});
